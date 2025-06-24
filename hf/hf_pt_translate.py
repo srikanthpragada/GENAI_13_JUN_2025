a2c811ca305351690931eebee3624941c38b27d5
srikanthpragada <srikanthpragada@gmail.com>
2025-06-24 17:26:42 +0530

    24-JUN

---
+++ hf/hf_pt_translate.py
@@ -0,0 +1,24 @@
+from huggingface_hub import InferenceClient
+from langchain.prompts import PromptTemplate
+import keys
+
+model_id = "mistralai/Mistral-7B-Instruct-v0.3"   
+client = InferenceClient(model=model_id, token= keys.HUGGINGFACE_KEY, provider="hf-inference")
+
+template_en_to_hi = """Translate the following English text to simple Hindi:
+
+{text}
+
+"""
+prompt_en_to_hi = PromptTemplate.from_template(template=template_en_to_hi)
+
+
+english_text = "What did you learn about AI?"
+
+prompt_en_to_hi = prompt_en_to_hi.format(text=english_text)
+print(prompt_en_to_hi)
+
+response = client.text_generation(prompt_en_to_hi)
+
+print(f"English: {english_text}")
+print(f"Hindi: {response}")
---
+++ hf/keys.py
@@ -0,0 +1,9 @@
+import os
+hugging_face_key = os.getenv('HF_KEY')
+HUGGINGFACE_KEY = hugging_face_key 
+
+if __name__ == '__main__':
+     print(HUGGINGFACE_KEY)
+
+
+
